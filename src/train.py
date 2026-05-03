02fbf9f5e3c19f1800d45cfb54c792f9cd98b246
Arjun krishnaa <arjunkrishnaa23@gmail.com>
2026-05-03 21:40:53 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,46 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, models
 
+train_gen = ImageDataGenerator(rescale=1./255)
+test_gen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_gen.flow_from_directory(
+    "dataset/train",
+    target_size=(100, 100),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+test_data = test_gen.flow_from_directory(
+    "dataset/test",
+    target_size=(100, 100),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(100,100,3)),
+    layers.MaxPooling2D(2,2),
+
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+
+    layers.Conv2D(128, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(3, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+model.fit(train_data, epochs=10, validation_data=test_data)
+
+model.save("model/fruit_model.h5")
+
+print("Model trained and saved!")
